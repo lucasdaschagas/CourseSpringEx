0661432a993922dd9ce51a36a4306ffa663d21e2
Lucas Souza <lucas.souza@FLEX.LOCAL>
2023-02-08 16:13:54 -0300

    creation of service class, creation of methods and endpoints

--- src/main/java/com/educandoweb/course/controller/UserController.java
+++ src/main/java/com/educandoweb/course/controller/UserController.java
@@ -2,25 +2,40 @@ package com.educandoweb.course.controller;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repository.UserRepository;
+import com.educandoweb.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
     @Autowired
-    private UserRepository repository;
+    private UserService service;
 
     @GetMapping
-    public ResponseEntity<User> findAll()
+    public ResponseEntity<List<User>> findAll()
     {
-        User u = new User("Maria","maria@gmail.com","99999999","12347");
-        System.out.println("Ok" + u);
+        List<User> list = service.findAll();
 
-        return ResponseEntity.ok().body(u);
+        return ResponseEntity.ok().body(list);
     }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+
+
+
+
 
 }
