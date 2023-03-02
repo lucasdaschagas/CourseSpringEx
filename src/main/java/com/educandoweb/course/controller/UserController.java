d5e0e26903e23a101529309174e7a47557673342
Lucas Souza <lucas.souza@FLEX.LOCAL>
2023-03-02 11:56:46 -0300

    Post test

--- src/main/java/com/educandoweb/course/controller/UserController.java
+++ src/main/java/com/educandoweb/course/controller/UserController.java
@@ -5,11 +5,10 @@ import com.educandoweb.course.repository.UserRepository;
 import com.educandoweb.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -31,6 +30,13 @@ public class UserController {
 
         User obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> saveUser(@RequestBody User obj){
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 
 
